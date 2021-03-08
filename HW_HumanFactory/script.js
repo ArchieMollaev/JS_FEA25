da81f034babfee22df41fa53eb5db70308a3a602
Oleksii Sakun <leha.sakun@gmail.com>
2021-03-08 23:51:27 +0200

    test

--- HW_HumanFactory/script.js
+++ HW_HumanFactory/script.js
@@ -13,23 +13,86 @@
 // vasya.sayHi();
 
 ///////////////Calculator///////////
-const num1 = document.getElementById("num1");
-const num2 = document.getElementById("num2");
-const result = document.getElementById("result");
+// const num1 = document.getElementById("num1");
+// const num2 = document.getElementById("num2");
+// const result = document.getElementById("result");
 
-function selectedOperation(event) {
-  result.value = "";
+// function selectedOperation(event) {
+//   result.value = "";
 
-  if (event.value === "+") {
-    result.value = Number(num1.value) + Number(num2.value);
-  }
-  if (event.value === "-") {
-    result.value = Number(num1.value) - Number(num2.value);
-  }
-  if (event.value === "*") {
-    result.value = Number(num1.value) * Number(num2.value);
-  }
-  if (event.value === "/") {
-    result.value = Number(num1.value) / Number(num2.value);
+//   if (event.value === "+") {
+//     result.value = Number(num1.value) + Number(num2.value);
+//   }
+//   if (event.value === "-") {
+//     result.value = Number(num1.value) - Number(num2.value);
+//   }
+//   if (event.value === "*") {
+//     result.value = Number(num1.value) * Number(num2.value);
+//   }
+//   if (event.value === "/") {
+//     result.value = Number(num1.value) / Number(num2.value);
+//   }
+// }
+//////////////Factory Pattern////////////////
+class ITCompany {
+  create(employeeType) {
+    if (employeeType === "Programmer") {
+      return new Programmer(employeeType);
+    }
+    if (employeeType === "Tester") {
+      return new Tester(employeeType);
+    }
   }
 }
+
+class Human {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  static getType() {
+    console.log("Человек");
+  }
+  getName() {
+    console.log(this.name);
+  }
+  getAge() {
+    console.log(this.age);
+  }
+}
+
+// const test = new Human("Vasya", 35);
+// console.log(test);
+console.log(Human.getType());
+
+class Programmer extends Human {
+  constructor(name, age, job) {
+    name = "Vasya";
+    age = 27;
+    job = "Programmer";
+    super(name, age, job);
+  }
+  static getJob() {
+    console.log(this.job);
+  }
+}
+
+class Tester extends Human {
+  constructor(name, age, job) {
+    name = "Petya";
+    age = 28;
+    job = "Tester";
+    super(name, age, job);
+  }
+  static getJob() {
+    console.log(this.job);
+  }
+}
+
+const company = new ITCompany();
+const employees = [];
+employees.push(company.create("Programmer", 27));
+employees.push(company.create("Tester", 33));
+console.log(employees);
+console.log(Tester.getJob()); ///////возвращает undefined
+console.log(Programmer.getJob()); ///////возвращает undefined
